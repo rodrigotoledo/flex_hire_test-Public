09a8bf304c4d2ccf9f478692b5e83a4b8999fe7b
rtoledo_dev <rodrigo@rtoledo.inf.br>
2024-01-04 13:27:57 -0300

    - working always with api from rails

--- app/jobs/page.js
+++ app/jobs/page.js
@@ -1,42 +1,42 @@
 import Link from 'next/link';
-import MockJob from '@/lib/mock_job';
 import graphqlQuery from '@/lib/api/graphqlQuery';
+import MockUser from '@/lib/mock_user';
 
 async function Jobs() {
-  const jobs = await MockJob.fakeData();
+  const userData = await MockUser.fakeData()
   try {
-    const { jobsData } = await graphqlQuery();
+    const { userData } = await graphqlQuery();
   } catch (error) {
     console.error('TODO: I have problems to get data in the api endpoint:', error);
-    const jobsData = false;
+    const userData = false;
   }
   
   return (
     <>
       <div className="w-full my-4 text-4xl font-bold">{JOBS_TITLE}</div>
       <Link href="/" className="underline">{BACK_TO_MAIN_PAGE}</Link>
-      {jobs ? (
+      {!userData ? (
         <div className="flex flex-col md:flex-row p-4 space-y-4 md:space-y-0 space-x-10">
           <div className="md:w-1/2 p-4 text-center align-center flex flex-col">
             <div className="w-32 h-32 md:w-60 md:h-60 bg-gray-300 rounded-full mx-auto mb-4 shadow-lg">
               <img
-                src={jobs.data.currentUser.avatarUrl}
+                src={userData.data.currentUser.avatarUrl}
                 className="m-auto w-60 h-60"
-                alt={jobs.data.currentUser.name}
+                alt={userData.data.currentUser.name}
               />
             </div>
-            <h2 className="text-2xl font-bold">{jobs.data.currentUser.name}</h2>
-            <p className="text-gray-600">{PHONE_LABEL}: {jobs.data.currentUser.phone}</p>
-            <p className="text-gray-600">{UPDATED_AT_LABEL}: {jobs.updatedAt}</p>
-            <p className="text-sky-500">{STATUS_LABEL}: {jobs.data.currentUser.status}</p>
+            <h2 className="text-2xl font-bold">{userData.data.currentUser.name}</h2>
+            <p className="text-gray-600">{PHONE_LABEL}: {userData.data.currentUser.phone}</p>
+            <p className="text-gray-600">{UPDATED_AT_LABEL}: {userData.updatedAt}</p>
+            <p className="text-sky-500">{STATUS_LABEL}: {userData.data.currentUser.status}</p>
           </div>
           <div className="bg-white rounded-md p-4 md:w-1/2">
             <div className="text-left">
-              {jobs.data.currentUser.jobs.length === 0 ? (
+              {userData.data.currentUser.allJobs.length === 0 ? (
                 <h4 className="text-left">{NO_JOBS_OBTAINED_LABEL}</h4>
               ) : (
                 <>
-                  {jobs.data.currentUser.jobs.map((job, index) => (
+                  {userData.data.currentUser.allJobs.map((job, index) => (
                     <div key={index} className="border-b p-2 mb-2 shadow">
                       <span className="font-bold">{HIRING_MANAGER_LABEL}:</span> {job.hiringManager}<br />
                       <span className="font-bold">{COMPANY_LABEL}:</span> {job.company}<br />
